9c03059f8c98a2780102084375976e2cb571e4a7
gilsegev99 <gilsegev@LAPTOP-0JJ0R1CI.>
2025-03-26 08:38:44 +0000

    Set up terraform config

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# local-air-quality
+## local-air-quality
+
+
+# Setup Infrastructure
+
+- Make sure Terraform is installed using (terraform instructions)
+- Change the credentials variable in variables.tf to the location of your service account credentials
+- Run terraform init in the terraform/ directory
+- Run terraform plan to confirm that the correct resources will be created
+- Run terraform apply
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "credentials" {
+  description = "Local Air Quality Credentials"
+  default     = "/home/gilsegev/keys/local-air-quality.json"  
+}
+
+
+variable "project" {
+  description = "Project"
+  default     = "local-air-quality-454807"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "europe-west2"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "EU"
+}
+
+variable "bq_dataset_name" {
+  description = "My Local Air Quality dataset"
+  default     = "local_air_quality"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Local Air Quality bucket"
+  default     = "local-air-quality-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
